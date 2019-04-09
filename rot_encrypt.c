c28ce3697ccd2cb2553d18e1f456dbdcb13169ac
Bella Sherring <isabella.sherring@uon.edu.au>
2019-04-09 03:21:55 +0000

    keeps the values within capital letters and not other bullshit characters!!!!!!!!

--- rot_encrypt.c
+++ rot_encrypt.c
@@ -13,7 +13,7 @@ int main()
     float key;
 
     //getting the phrase
-    printf("enter a phrase to encode: \n");     //prompts phrase to be put in by user
+    printf("enter a phrase to encode in capitals: \n");     //prompts phrase to be put in by user
     scanf("%s", phrase);                        //scans the phrase from the user
     
     //allows user to choose a shifting value, the 'key'
@@ -33,7 +33,18 @@ char encrypt(char *phrase, float key) // encoding function definition
     char i=0;
     while(phrase[i] != 0)
     {
-        phrase[i] = phrase[i] + key;    //shifts it by the key. wont keep within capital letters though - any ascii character, not just letters
+        if((phrase[i]+key)>=65 && (phrase[i]+key)<=90) //if it ends up in the capital letter range dont change it
+        {
+            phrase[i] = phrase[i] + key;
+        }
+        else if((phrase[i]+key)>90) //if it ends up above the range, take away 26 and then add the key
+        {
+            phrase[i] = phrase[i] - 26 + key;
+        }
+        else if((phrase[i]+key)<65) //if it ends up below the range, add 26 and then the key
+        {
+            phrase[i] = phrase[i] + 26 + key;
+        }
         printf("%c", phrase[i]);
         i++;
     }
